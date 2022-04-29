1f8dfe12e5c95801ca92e13c66c60fd522fccbaa
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-04-29 10:49:48 +0530

    check

--- src/App.js
+++ src/App.js
@@ -3,11 +3,62 @@ import './App.css';
 
 function App() {
 
-let arr =[50,"mitul",40,30,20,10];
+
+  let Medicine  = [
+    { 
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
+    },
+    {
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+   
+  ];
+
+  console.log(Medicine.map((i) => {return i}));  // MAP
+
+  console.log(Medicine.filter((g) => {return g.expiry >=2022})); // FILTER
+
+  // console.log(Medicine.);
 
 
 
 
+
+  
 
 
 

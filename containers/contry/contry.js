9dbe670fced12cf2eb3cbaec0b9ae769c2e5721a
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-05-04 10:26:46 +0530

    comp

--- containers/contry/contry.js
+++ containers/contry/contry.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 
-
-
-
-export default class contry extends Component {
+class contry extends Component {
 
     constructor(props) {
         super(props);
         
-        this.state ={
-            contry: 'india'
-        }; 
+       this.state = {
+           contry:'india'
+       } 
     }
-
-
     
+
+
     render() {
         return (
-           <>
-            <div>Contry: {this.state.contry}</div>
-           
-           
-           </>
+            
+            <div>Contry:{this.state.contry}</div>
+       
         );
     }
 }
 
+export default contry;
